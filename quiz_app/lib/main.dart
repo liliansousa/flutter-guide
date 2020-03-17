6fa63fc83e1df24ad61a310276692f5b27d5f76b
Lilian Dias <liliandias.dev@gmail.com>
2020-03-17 12:42:26 -0300

    Scaffold Init base setup

--- quiz_app/lib/main.dart
+++ quiz_app/lib/main.dart
@@ -1,13 +1,22 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text('Hello World'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Quiz App'),
+          actions: <Widget>[],
+        ),
+        body: Column(
+          children: <Widget>[
+            Text('Text')
+          ],
+        ),
+      ),
     );
   }
 }
